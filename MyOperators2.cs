1a8770ceca2469a174a8dffee6d5bfc6192b29bf
hsuehyt <hsueh.yuting@gmail.com>
2024-04-11 22:07:55 +0800

    string

--- MyOperators2.cs
+++ MyOperators2.cs
@@ -51,7 +51,11 @@ public class MyOperators3 : MonoBehaviour
         int largeNumber = 1_000_000;
         byte smallNumber = (byte)largeNumber;
 
-        TextMeshPro.text = $"anyName: {anyName} anyName2: {anyName2} anyName3: {anyName3} anyName4: {anyName4} anyName5: {anyName5} anyName6: {anyName6} anyName7:{anyName7} solution:{solution} conversion:{conversion} largeNumber:{largeNumber} smallNumber:{smallNumber}";
+        string firstName = "Y";
+        string lastName = "H";
+        string fullName = firstName + " " + lastName;
+
+        TextMeshPro.text = $"anyName: {anyName} anyName2: {anyName2} anyName3: {anyName3} anyName4: {anyName4} anyName5: {anyName5} anyName6: {anyName6} anyName7:{anyName7} solution:{solution} conversion:{conversion} largeNumber:{largeNumber} smallNumber:{smallNumber} name:{fullName}";
 
 
 
